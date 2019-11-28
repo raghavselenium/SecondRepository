9262d69096ec3d04f39c0d102a02a2db1c85f129
raghavselenium <raghav.n82@gmail.com>
2019-11-28 09:43:38 +0530

    2 projects are added to secondrepository

---
+++ 745amProject/src/firstpackage/thirdclass.java
@@ -0,0 +1,10 @@
+package firstpackage;
+
+public class thirdclass {
+
+	public static void main(String[] args) {
+		System.out.println("Third Class");
+
+	}
+
+}
---
+++ 7amProject/src/praticepackage/secondclass.java
@@ -0,0 +1,10 @@
+package praticepackage;
+
+public class secondclass {
+
+	public static void main(String[] args) {
+		System.out.println("Second Class");
+
+	}
+
+}
