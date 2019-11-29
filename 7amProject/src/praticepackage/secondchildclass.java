761fc34d77109e5097f946e8355e1902780a6711
raghavselenium <raghav.n82@gmail.com>
2019-11-29 08:37:18 +0530

    added second line in secondchildclass of 7amProject

--- 7amProject/src/praticepackage/secondchildclass.java
+++ 7amProject/src/praticepackage/secondchildclass.java
@@ -5,6 +5,7 @@ public class secondchildclass {
 	public static void main(String[] args) 
 	{
 		System.out.println("this is child of second class");
+		System.out.println("second message this is child of second class");
 
 	}
 
